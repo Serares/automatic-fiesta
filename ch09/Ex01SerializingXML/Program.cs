0954f463684a6631e9c23e3d29323ca19ad769bf
Jianu Rares <Rares>
2024-12-13 13:43:24 +0200

    Added xml conversion ex

---
+++ ch09/Ex01SerializingXML/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Xml.Serialization; // To use XmlSerializer.
+
+
+using MyLibrary.Shared;
+
+List<Shape> listOfShapes = new() {
+    new Circle {Color="Red", Radius = 19.3M},
+    new Rectangle {Color="Blue", Height= 12.3M, Width = 18.9M},
+    new Circle {Color="Blue", Radius = 8.2M}
+};
+
+
+XmlSerializer xs = new(type: listOfShapes.GetType());
+
+string pathToFile = Combine(CurrentDirectory, "shapes.xml");
+
+WriteLine("Serializing to XML {0}",
+arg0: pathToFile
+);
+using (FileStream stream = File.Create(pathToFile))
+{
+    xs.Serialize(stream, listOfShapes);
+}
+
+WriteLine("Desirialize XML {0}",
+arg0: pathToFile
+);
+
+using (FileStream xmlLoad = File.Open(pathToFile, FileMode.Open))
+{
+    List<Shape>? list = xs.Deserialize(xmlLoad) as List<Shape>;
+
+
+    if (list is not null)
+    {
+        foreach (Shape sh in list)
+        {
+            WriteLine("Shape: {0} \n color {1} \n Area: {2}",
+            arg0: sh.GetType(),
+            arg1: sh.Color,
+            arg2: sh.Area
+            );
+        }
+    }
+}
---
+++ ch09/Ex01SerializingXML/Shape.cs
@@ -0,0 +1,47 @@
+namespace MyLibrary.Shared;
+using System.Xml.Serialization; // To use [XmlAttribute].
+[XmlInclude(typeof(Circle))]
+[XmlInclude(typeof(Rectangle))]
+public class Shape
+{
+    public Shape() { }
+    [XmlAttribute("color")]
+    public string? Color { get; set; }
+    public virtual string? Area { get; }
+}
+
+public class Rectangle : Shape
+{
+    public Rectangle() { }
+    [XmlAttribute("height")]
+    public decimal Height { get; set; }
+
+    [XmlAttribute("width")]
+    public decimal Width { get; set; }
+    public override string Area
+    {
+        get
+        {
+            decimal area = Height * Width;
+            return area.ToString();
+        }
+    }
+}
+
+public class Circle : Shape
+{
+    public Circle() { }
+    [XmlAttribute("radius")]
+    public decimal Radius { get; set; }
+
+    public override string Area
+    {
+        get
+        {
+            double r = (double)Radius;
+            double area = Math.PI * Math.Pow(r, 2);
+
+            return area.ToString();
+        }
+    }
+}
