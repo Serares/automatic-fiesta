1352b38db0e28831f77789df6c47bd99247ee3d9
Jianu Rares <Rares>
2024-12-14 13:20:41 +0200

    Add CRUD queries in the modifications partial class

--- ch10/EFCore/Program.cs
+++ ch10/EFCore/Program.cs
@@ -12,8 +12,24 @@ Trace.AutoFlush = true;
 // using NorthwindDb db = new();
 // WriteLine("Provider {0}", arg0: db.Database.ProviderName);
 ConfigureConsole();
-QueryingCategories();
+// QueryingCategories();
 // FilteredIncludes();
 // QueryingProducts();
 // GettingOneProduct();
 // QueryingWithLike();
+var resultAdd = AddProduct(categoryId: 6,
+productName: "Bob's Burgers", price: 500M, stock: 72);
+if (resultAdd.affected == 1)
+{   
+    /* 💡
+    State: Added, ProductId: 0
+State: Unchanged, ProductId: 78
+Add product successful with ID: 78.
+    📝
+    When the new product is first created in memory and tracked by the EF Core change
+tracker, it has a state of Added and its ID is 0. After the call to SaveChanges, it has a state
+of Unchanged and its ID is 78, the value assigned by the database.
+*/
+    WriteLine($"Add product successful with ID: {resultAdd.productId}.");
+}
+ListProducts(productIdsToHighlight: new[] { resultAdd.productId });
